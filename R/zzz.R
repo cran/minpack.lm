e960b7515c4939526d68ea2f1f75d418f0cd96ae
Katharine M. Mullen <kate@few.vu.nl>
2008-03-14 00:00:00 +0000

    version 1.0-9

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,14 @@
 	library.dynam(pack, pack, lib)
 
 }
+
+".onAttach" <- function (lib, pack)
+{
+  cat("
+This product includes software developed by the University of Chicago, as
+Operator of Argonne National Laboratory.
+
+See the LICENSE file distributed with the minpack.lm source code or
+http://www.netlib.org/minpack/disclaimer for the full license.\n\n")
+
+}
